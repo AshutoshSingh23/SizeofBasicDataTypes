3d9c8d2b45c9a7adb424390d3bba8aa08b72c4ca
AshutoshSingh23 <63116827+AshutoshSingh23@users.noreply.github.com>
2020-04-13 17:40:23 +0530

    Created SizeofDataTypes.c
    It prints the size of char, int, float and double

---
+++ SizeofDataTypes.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char c;
+    int i;
+    float f;
+    double d;
+     cout << "Size of char: " << sizeof(c) << " byte" << endl;
+    cout << "Size of int: " << sizeof(i) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(f) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(d) << " bytes" << endl;;
+}
